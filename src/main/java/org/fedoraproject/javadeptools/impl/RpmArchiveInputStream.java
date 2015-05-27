1ff86d6de5cbbe04ff8dc818fcbc48851be176f1
Michael Simacek <msimacek@redhat.com>
2015-05-27 15:13:32 +0200

    Move RpmArchiveInputStream

--- src/main/java/org/fedoraproject/javadeptools/impl/RpmArchiveInputStream.java
+++ src/main/java/org/fedoraproject/javadeptools/impl/RpmArchiveInputStream.java
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2012 Red Hat, Inc.
+ * Copyright (c) 2012-2015 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.fedoraproject.javadeptools;
+package org.fedoraproject.javadeptools.impl;
 
 import java.io.File;
 import java.io.IOException;
