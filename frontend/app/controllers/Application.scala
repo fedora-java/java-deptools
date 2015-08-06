4b1a827e81ec210e9281af2fbc037d6788d4457c
Michael Simacek <msimacek@redhat.com>
2015-08-06 18:40:08 +0200

    Configurable frontend DB path

--- frontend/app/controllers/Application.scala
+++ frontend/app/controllers/Application.scala
@@ -7,6 +7,7 @@ import org.fedoraproject.javadeptools.impl.DatabaseFactory
 import org.fedoraproject.javadeptools.Database
 import org.fedoraproject.javadeptools.Query
 import play.api.mvc.Request
+import play.api.Play
 
 object Page {
   val itemsPerPage = 100
@@ -26,7 +27,7 @@ case class Page[T](content: Iterable[T], currentPage: Int, totalCount: Int) {
 
 object Application extends Controller {
 
-  lazy val dbFactory = new DatabaseFactory("jdbc:h2:tcp://localhost/~/test")
+  lazy val dbFactory = new DatabaseFactory(Play.current.configuration.getString("java-deptools.db.url").get)
 
   def index(page: Int, q: String) = Action { implicit request =>
     val db = dbFactory.createDatabase()
