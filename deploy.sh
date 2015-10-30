bd75b4f67553a9726cb14b2531d564be70c5d7a3
Michael Simacek <msimacek@redhat.com>
2015-10-30 09:18:27 +0100

    Deploy both core and frontend

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -e
 shell='ssh root@java-deptools.fedorainfracloud.org'
-activator frontend/rpm:packageBin
+activator core/rpm:packageBin frontend/rpm:packageBin
+tar cj {core,frontend}/target/rpm/RPMS/noarch/java-deptools-*-0-1.noarch.rpm \
+       ./././././generate-repos.sh | \
 $shell '
 set -e
+tar xj  --strip-components 5
 systemctl stop java-deptools-frontend.service ||:
-dd of=frontend.rpm
-dnf install -y frontend.rpm
-systemctl start java-deptools-frontend.service' \
-    < frontend/target/rpm/RPMS/noarch/java-deptools-frontend-0-1.noarch.rpm
+dnf install -y java-deptools-{core,frontend}-0-1.noarch.rpm
+rm -f java-deptools-{core,frontend}-0-1.noarch.rpm
+mv generate-repos.sh /usr/libexec/
+systemctl start java-deptools-frontend.service'
