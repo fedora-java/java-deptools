b519c71d9d2eb86d986735855a530e9edd4a9ead
Michael Simacek <msimacek@redhat.com>
2015-11-02 16:50:43 +0100

    Fix deployment of bootstrap submodule

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,11 @@ set -e
 VERSION=0
 RELNO="$(git rev-list "$(git describe --tags --abbrev=0)..HEAD" --count)"
 shell='ssh root@java-deptools.fedorainfracloud.org'
-git archive HEAD --prefix="java-deptools-$VERSION/" | gzip > java-deptools-$VERSION.tar.gz
-mkdir -p build
+cmd='git ls-tree -r HEAD |awk "/ blob /{print\$4}"'
+{ eval "$cmd" && git submodule foreach "$cmd | sed s@^@\$path/@"; } | \
+    sed '/^Entering /d' | xargs tar --xform "s@^@java-deptools-$VERSION/@S" -cf java-deptools-$VERSION.tar.gz
+rm -r build
+mkdir build
 cd build
 sed "s/^Release:[^%]*/&.$RELNO/" ../java-deptools.spec > java-deptools.spec
 rpmbuild -bb -D"_sourcedir $PWD/.." -D"_rpmdir $PWD" java-deptools.spec
