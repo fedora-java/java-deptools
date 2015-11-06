0f4450db392af5350715f75adc8d9bf320a0d2bc
Michael Simacek <msimacek@redhat.com>
2015-11-06 16:31:31 +0100

    Set q2 field as disabled when invisible

--- frontend/public/javascripts/main.js
+++ frontend/public/javascripts/main.js
@@ -14,7 +14,7 @@ function changeQueryType() {
             "classes": ["Class name:"],
             "manifests": ["Header:", "Value:"]
     }[$("#qtype")[0].value];
-    $("#q2").css("display", transition[1]? "inline": "none");
+    $("#q2").css("display", transition[1]? "inline": "none").prop("disabled", !transition[1]);
     $("label[for='q']").text(transition[0]);
     $("label[for='q2']").text(transition[1] || "");
 }
